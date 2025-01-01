7f1531dce936f54212fd7ebcabe42016c3bb3a84
𝗽𝗿𝗮𝘁𝗵 ★ <134331217+prathamdby@users.noreply.github.com>
2025-01-01 23:35:42 +0530

    Time: 4 ms (76.74%), Space: 44.6 MB (47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,34 +3,12 @@ class Solution {
         if (s.length() != t.length())
             return false;
 
-        HashMap<Character, Integer> visited = new HashMap<>();
-
         char[] sChars = s.toCharArray();
         char[] tChars = t.toCharArray();
 
-        for (char ch : sChars) {
-            if (visited.containsKey(ch))
-                visited.put(ch, visited.get(ch) + 1);
-            else
-                visited.put(ch, 1);
-        }
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
 
-        for (char ch : tChars) {
-            if (visited.containsKey(ch))
-                if (visited.get(ch) > 0)
-                    visited.put(ch, visited.get(ch) - 1);
-                else
-                    return false;
-        }
-
-        boolean anagram = true;
-        for (Integer count : visited.values()) {
-            if (count != 0) {
-                anagram = false;
-                break; // No need to check further
-            }
-        }
-
-        return anagram;
+        return Arrays.equals(sChars, tChars);
     }
 }

ec2de7077d8cdcd9354014e3067b427fffc718de
𝗽𝗿𝗮𝘁𝗵 ★ <134331217+prathamdby@users.noreply.github.com>
2025-01-28 20:45:26 +0530

    Time: 4 ms (100%), Space: 44.5 MB (79.87%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -1,9 +1,9 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
         Set<String> uniqueEmails = new HashSet<>();
-        for (String email : emails) {
+        for (String email : emails)
             uniqueEmails.add(normalizeEmail(email));
-        }
+
         return uniqueEmails.size();
     }
 
@@ -13,10 +13,11 @@ class Solution {
 
         for (int index = 0; index < domainIndex; index++) {
             char ch = email.charAt(index);
-            if (ch == '+')
-                break;
-            if (ch != '.')
-                sb.append(ch);
+
+            if (ch == '+') break;
+            if (ch == '.') continue;
+
+            sb.append(ch);
         }
 
         sb.append(email.substring(domainIndex));
